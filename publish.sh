6992657db70773789a27d5bb5dd95adc4d6221be
Bin Cheng <bin.cheng@neclab.eu>
2019-04-30 11:54:00 +0200

    add the script to build the docker images for raspberry pi

--- publish.sh
+++ publish.sh
@@ -1,3 +1,5 @@
+# amd64 ubuntu version
+
 docker push  fogflow/discovery:intent
 
 docker push  fogflow/broker:intent
@@ -7,6 +9,18 @@ docker push  fogflow/master:intent
 docker push  fogflow/worker:intent
 
 docker push  fogflow/designer:intent
+
+# arm version for raspberry pi
+
+docker push  fogflow/discovery:arm.intent
+
+docker push  fogflow/broker:arm.intent
+
+docker push  fogflow/master:arm.intent
+
+docker push  fogflow/worker:arm.intent
+
+docker push  fogflow/designer:arm.intent
 
 
 
